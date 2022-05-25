6d31251957b9b082590dd13ff01ebb9921f97416
Dave <avcasalderrey@gmail.com>
2022-05-25 18:17:08 +0200

    Aún decidiendo la estructura del proyecto

--- proyectodam/src/main/java/proyectodam/Especie.java
+++ proyectodam/src/main/java/proyectodam/Especie.java
@@ -1,5 +1,13 @@
 package proyectodam;
 
+import javax.swing.ImageIcon;
+
 public class Especie {
     
+    private String nombre;
+    private int[] statsBase; //HP, Atk, Def, AtkSp, DefSp, Speed
+    private ImageIcon imagenFrente, imagenEspalda, imagenIcono;
+    private Movimientos[] setMovimientos;
+    private Tipo[] tipo;
+
 }
--- proyectodam/src/main/java/proyectodam/Pokemon.java
+++ proyectodam/src/main/java/proyectodam/Pokemon.java
@@ -4,18 +4,19 @@ import javax.swing.ImageIcon;
 
 
 // https://github.com/Jmgiacone/Pokemon
+
 public class Pokemon {
     
     private String apodo;
     private Especie especie;
     private boolean[] estado; //Envenenado, Paralizado, Quemado, Congelado, Dormido
     private int nivel;
-    private int[] estadisticas; //HP, Atk, Def, SpAtk, SpDef, Speed
-    private ImageIcon imagenFrente, imagenEspalda, imagenIcono;
-    private Movimientos[] setMovimientos;
+    private int[] statsCombate; //HP, Atk, Def, AtkSp, DefSp, Speed
 
-    public Pokemon(String apodo, Especie especie){
-
+    public Pokemon(String apodo, Especie especie, int nivel){
+        this.apodo = apodo;
+        this.especie = especie;
+        this.nivel = nivel;
     }
 
 }
